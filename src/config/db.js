5529cb4d1801f54a9166bb9bc321b669bc3ca500
Momar TALL <tonemail@gmail.com>
2026-01-22 03:15:16 +0000

    fix db

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,6 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-});
+const db = mysql.createPool(process.env.DATABASE_URL);
 
 module.exports = db;
