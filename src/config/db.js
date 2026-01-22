a2f3e7581842e548371a4b9bbc88aab44367aba3
Momar TALL <tonemail@gmail.com>
2026-01-22 03:43:19 +0000

    fix db 2

--- src/config/db.js
+++ src/config/db.js
@@ -1,24 +1,20 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-let db;
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT || 3306),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
 
-if (process.env.DATABASE_URL) {
-  // ✅ Railway / Render URL format: mysql://user:pass@host:port/db
-  db = mysql.createPool(process.env.DATABASE_URL);
-} else {
-  // ✅ Variables séparées
-  db = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT || 3306),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
-  });
-}
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
 module.exports = db;
